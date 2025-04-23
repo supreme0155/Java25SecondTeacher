3e1ce895b43ad74547c247dc5a822d7e8b7345cb
supreme0155 <shin7143@gmail.com>
2025-04-23 11:25:51 +0800

    test

--- src/App.java
+++ src/App.java
@@ -1,7 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! I am Edward No. 123!");
 
         System.out.println("Hello, World! I am Edward No. 17!");
+    }
+List<Integer> numbers = new ArrayList<>();
+for (int i = 1; i <= 30; i++) {
+    if (i != 16 && i != 18) {
+        numbers.add(i);
+    }
+}
+
+Collections.shuffle(numbers);
+
+for (int i = 0; i < numbers.size(); i += 2) {
+    if (i + 1 < numbers.size()) {
+        System.out.println("Pair: " + numbers.get(i) + " and " + numbers.get(i + 1));
+    } else {
+        System.out.println("Unpaired: " + numbers.get(i));
     }
 }
