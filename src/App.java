880aff33333597a4a194d3dee4ceaafc3b6a5432
supreme0155 <shin7143@gmail.com>
2025-04-23 10:42:38 +0800

    變更

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 11!");
+        System.out.println("Hello, World! I am Edward No. 123!");
 
         System.out.println("Hello, World! I am Edward No. 17!");
     }
