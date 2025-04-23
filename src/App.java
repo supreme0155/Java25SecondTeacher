c3557325ae7697ec476284df07fb26b3426f6fed
supreme0155 <107744432+supreme0155@users.noreply.github.com>
2025-04-23 13:30:04 +0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 123!");
-
-        System.out.println("Hello, World! I am Edward No. 17!");
+            System.out.println("Hello, World!");
     }
 List<Integer> numbers = new ArrayList<>();
 for (int i = 1; i <= 30; i++) {
